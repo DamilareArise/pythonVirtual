97f8b389166c49877fb5d84520faac56574c54aa
Arise Damilare <arisedamilare5@gmail.com>
2025-03-22 14:00:32 +0100

    Add initial Python script with comments and variable examples

---
+++ index.py
@@ -0,0 +1,110 @@
+# print(50+50)
+# print("Hello world")
+
+# Python commentting
+# 1. comment is used for debugging
+# 2. to make set codes not to be executed
+# 3. for documentation
+'''
+block comment / doc string 
+
+ajsbasas
+masaknalsa
+djahshashlahas
+ajsbaksaslda
+'''
+
+# Python variable 
+
+# name = "Shola"
+# print(name)
+
+bottle = "water"
+
+# 1. Variable name
+# 2. Assignment operator (=)
+# 3. Value
+
+# Rules guiding variable declaration
+'''
+1. A variable name can only begin with an alphabet or underscore
+2. A variable name can only contain alphabet, number, and underscore. no special character and space instead you can use;
+    i. pascal casing e.g PascalCasing
+    ii. camel casing e.g camelCasing
+    iii. snake casing e.g snake_casing
+3. A variable name cannot be a keyword
+4. A variable name must be descriptive
+'''
+
+# firstName = 'Damilare'
+
+# Types of variable declaration
+# 1. single variable single value
+name = 'Dami'
+# 2. single variable multiple value
+names = 'Damilare', 'Shola', 'Tosin'
+# print(names)
+# 3. multiple variable single value
+x = y = z = 30
+# print(x, y, z)
+# 4. multiple variable multiple value
+x, y, z = 30, 40, 50
+
+
+product_name = 'LG TV'
+manufacturer = 'LG'
+price = 50.45
+quantity = 10
+
+# print(product_name, manufacturer, price, quantity)
+# Concatenation -> The ability to joining two or more strings together
+
+# print("The name of the product is", product_name )
+# print("The name of the product is " + product_name )
+# print("The price is " + str(price))
+
+name = 'Adeshola okunola'
+age = 30
+nationality = 'Nigerian'
+study = 'data analysis'
+
+# print('My name is '+name+'. I am '+str(age)+'years old.'+' I am a '+nationality+' currently studying '+study)
+
+# f-string
+# print(f'My name is {name}. I am {age}years old. I am a {nationality}, currently studying {study}.')
+
+# Python DATATYPES
+'''
+1. Number Types; 
+    i. Integers int() e.g 12, 345, 1234
+    ii. Float float() e.g 12.5, 3.14, 0.0
+    iii. Complex complex() e.g 3+4j, 5-6j
+2. Text type; 
+    i. String str()
+
+3. Sequence Type:
+    i. List list() [] e.g [1, 2, 3], ['a', 'b', 'c']
+    ii. tuple tuple() () e.g (1, 2, 3), ('a', 'b', 'c')
+    iii. Range range() e.g range(1, 10)
+4. boolean bool(); True , False
+'''
+
+# number = 12.6 + 2j
+# print(type(number))
+
+# students = ('Ade', 'Ola', 'Femi')
+# print(type(students))
+
+# number = list(range(2, 10, 2))
+# print(number)
+
+# isMarried = False
+# print(type(isMarried))
+
+
+# Assignment
+'''
+1. Read up on Python Operators
+2. Read up on Conditional statement
+3. Build a simple caculator
+'''
